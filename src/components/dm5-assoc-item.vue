7b727b0c7402596895e5f06ace29b0491d818f9e
Jörg Richter <jri@deepamehta.de>
2020-10-25 20:59:04 +0100

    Assoc result lists: render assoc as colored line
    See dmx-platform/dmx-platform#406

--- src/components/dm5-assoc-item.vue
+++ src/components/dm5-assoc-item.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="dm5-assoc-item">
     <dm5-topic :topic="assoc.player1.topic"></dm5-topic>
+    <dm5-assoc-line :assoc="assoc"></dm5-assoc-line>
     <dm5-topic :topic="assoc.player2.topic"></dm5-topic>
   </div>
 </template>
@@ -15,7 +16,21 @@ export default {
   },
 
   components: {
-    'dm5-topic': require('./dm5-topic').default
+    'dm5-topic': require('./dm5-topic').default,
+    'dm5-assoc-line': require('./dm5-assoc-line').default
   }
 }
 </script>
+
+<style>
+.dm5-assoc-item {
+  display: flex;
+  align-items: center;
+}
+
+.dm5-assoc-item .dm5-assoc-line {
+  flex: auto;
+  margin-left: 16px;
+  margin-right: 16px;
+}
+</style>
---
+++ src/components/dm5-assoc-line.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dm5-assoc-line" :style="{'border-top-color': assoc.color}"></div>
+</template>
+
+<script>
+import dm5 from 'dmx-api'
+
+export default {
+
+  props: {
+    assoc: {type: dm5.Assoc, required: true}
+  }
+}
+</script>
+
+<style>
+.dm5-assoc-line {
+  border-top: 3px solid;
+  transform: rotate(-2deg);
+}
+</style>
