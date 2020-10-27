9c57e2364fb1714ddf4aaff71d0b21c2ec0fd589
Jörg Richter <jri@deepamehta.de>
2020-10-27 23:59:39 +0100

    Assoc list: show assoc value and role types

--- src/components/dm5-assoc-line.vue
+++ src/components/dm5-assoc-line.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="dm5-assoc-line" :style="{'background-color': assoc.color}">
-    <div class="aux-node"></div>
+  <div class="dm5-assoc-line" :style="lineStyle">
+    <div class="value label">{{assoc.value || '&nbsp;'}}</div>
+    <div class="aux-node" :style="lineStyle">&nbsp;</div>
+    <div class="role-types">
+      <div class="label">{{roleType1}}</div>
+      <div class="label">{{roleType2}}</div>
+    </div>
   </div>
 </template>
 
@@ -8,25 +13,62 @@
 import dm5 from 'dmx-api'
 
 export default {
+
   props: {
     assoc: {type: dm5.Assoc, required: true}
+  },
+
+  computed: {
+
+    lineStyle () {
+      return {
+        'background-color': this.assoc.color
+      }
+    },
+
+    roleType1 () {
+      return roleTypeLabel(this.assoc.player1)
+    },
+
+    roleType2 () {
+      return roleTypeLabel(this.assoc.player2)
+    }
   }
+}
+
+function roleTypeLabel (player) {
+  return player.roleTypeUri !== 'dmx.core.default' ? player.roleTypeName : ''
 }
 </script>
 
 <style>
 .dm5-assoc-line {
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
   height: 3px;
   transform: rotate(-2deg);
 }
 
+.dm5-assoc-line .value {
+  text-align: center;
+  margin-top: -20px;
+}
+
 .dm5-assoc-line .aux-node {
+  align-self: center;
   width: 6px;
   height: 6px;
   border-radius: 3px;
-  background-color: inherit;
+  margin-top: 3px;
+  margin-bottom: 2px;
+}
+
+.dm5-assoc-line .role-types {
+  display: flex;
+}
+
+.dm5-assoc-line .role-types div {
+  flex: auto;
+  text-align: center;
 }
 </style>
